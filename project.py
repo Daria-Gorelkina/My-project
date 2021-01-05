cded260ff6e01660ba0cb05252029553a56a577a
Даша <gorelkina.dari@yandex.ru>
2021-01-05 20:29:26 +0300

    Добавление кнопки "Правила игры"

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import random
+import pygame_gui
 
 import pygame
 
@@ -21,11 +22,50 @@ def load_image(name, colorkey=None):
     return image
 
 
-pygame.init()
-size = WIDTH, HEIGHT = 500, 500
-screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
+class Rules():
+    def __init__(self):
+        self.text = None
+        self.rules = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((20, 140), (120, 50)),
+            text='Правила игры',
+            manager=manager
+        )
+        self.back = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((20, 20), (50, 25)),
+            text='Назад',
+            manager=manager
+        )
+        self.back.hide()
 
+    def show_rules(self):
+        fon = pygame.transform.scale(load_image('fon.jpg'),
+                                     (WIDTH, HEIGHT))
+        screen.blit(fon, (0, 0))
+        self.rules.hide()
+        self.text = pygame_gui.elements.ui_text_box.UITextBox(
+            html_text='О нет! Земле угрожает опасность! Танос снова '
+                      'решил напасть на наш народ! Именно Вам '
+                      'предстоит спасти Землю от ужасного Таноса! '
+                      'Вперед! <br>Перед Вами'
+                      ' стоит 5 препядствий, которые нужно выполнить.'
+                      'Для паузы нажмите <u>Enter</u>, для сохранения'
+                      ' игры <u>Ctrl+S</u>.',
+            relative_rect=pygame.Rect((150, 100), (500, 400)),
+            manager=manager
+        )
+        self.back.show()
+
+    def hide_text(self):
+        self.text.hide()
+        self.back.hide()
+
+
+pygame.init()
+fl = False
+size = WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode(size)
+manager = pygame_gui.UIManager((800, 600))
+clock = pygame.time.Clock()
 
 FPS = 50
 
@@ -37,11 +77,13 @@ def terminate():
 
 def start_screen():
     intro_text = ["WAR OF THE WORLDS", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+                  "",
+                  "Нажмите PLAY для начала игры",
+                  "Восстановите игру"]
 
-    fon = pygame.transform.scale(load_image('fone.jpg'), (WIDTH, HEIGHT))
+    fon = pygame.transform.scale(load_image('fon.jpg'),
+                                 (WIDTH, HEIGHT))
+    rul = Rules()
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 100
@@ -55,12 +97,22 @@ def start_screen():
         screen.blit(string_rendered, intro_rect)
 
     while True:
+        time_delta = clock.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.KEYDOWN:
                 return True
+            elif event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == rul.rules:
+                        rul.show_rules()
+                    if event.ui_element == rul.back:
+                        rul.hide_text()
+                        start_screen()
+            manager.process_events(event)
+        manager.update(time_delta)
+        manager.draw_ui(screen)
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -73,7 +125,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # screen.fill((255, 25, 0))
+    screen.fill((235, 195, 155))
     # pygame.draw.rect(screen, (0, 0, 0), (20, 20, 50, 50), 1)
     pygame.display.flip()
 pygame.quit()
