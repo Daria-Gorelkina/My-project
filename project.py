7125062d1b75e0345bc09a98584980b331eccbb2
Даша <gorelkina.dari@yandex.ru>
2021-01-06 16:17:21 +0300

    Отлаживание кнопки "PLAY" и добавление курсора в игру

--- project.py
+++ project.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import random
 import pygame_gui
 
 import pygame
@@ -22,7 +21,7 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Rules():
+class Rules:
     def __init__(self):
         self.text = None
         self.rules = pygame_gui.elements.UIButton(
@@ -60,6 +59,16 @@ class Rules():
         self.back.hide()
 
 
+class Play:
+    def __init__(self):
+        self.play = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((20, 410), (100, 50)),
+            text='PLAY',
+            manager=manager,
+        )
+        self.play.show()
+
+
 pygame.init()
 fl = False
 size = WIDTH, HEIGHT = 800, 600
@@ -68,6 +77,28 @@ manager = pygame_gui.UIManager((800, 600))
 clock = pygame.time.Clock()
 
 FPS = 50
+
+
+class Cursor(pygame.sprite.Sprite):
+    image = load_image("cur.png")
+
+    def __init__(self, *group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно!!!
+        super().__init__(*group)
+        self.image = Cursor.image
+        self.rect = self.image.get_rect()
+        # self.rect.x = random.randrange(width-120)
+        # self.rect.y = random.randrange(height-120)
+
+    def update(self, coord):
+        self.rect.x = coord[0]
+        self.rect.y = coord[1]
+
+
+all_sprites = pygame.sprite.Group()
+
+Cursor(all_sprites)
 
 
 def terminate():
@@ -84,11 +115,7 @@ def start_screen():
     fon = pygame.transform.scale(load_image('fon.jpg'),
                                  (WIDTH, HEIGHT))
     rul = Rules()
-    play = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect((20, 410), (100, 50)),
-        text='PLAY',
-        manager=manager,
-    )
+    play1 = Play()
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 100
@@ -106,15 +133,37 @@ def start_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.USEREVENT:
+            if event.type == pygame.USEREVENT:
                 if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == play1.play:
+                        return True
                     if event.ui_element == rul.rules:
                         rul.show_rules()
                     if event.ui_element == rul.back:
                         rul.hide_text()
-                        start_screen()
-                    if event.ui_element == play:
-                        return True
+                        intro_text = ["WAR OF THE WORLDS", "",
+                                      "",
+                                      "Добро пожаловать в WAR OF "
+                                      "THE WORLDS",
+                                      "Нажмите PLAY для начала игры"]
+
+                        fon = pygame.transform.scale(
+                            load_image('fon.jpg'),
+                            (WIDTH, HEIGHT))
+                        rul = Rules()
+                        screen.blit(fon, (0, 0))
+                        font = pygame.font.Font(None, 30)
+                        text_coord = 100
+                        for line in intro_text:
+                            string_rendered = font.render(line, 1,
+                                                          pygame.Color(
+                                                              'white'))
+                            intro_rect = string_rendered.get_rect()
+                            text_coord += 15
+                            intro_rect.top = text_coord
+                            intro_rect.x = 15
+                            text_coord += intro_rect.height
+                            screen.blit(string_rendered, intro_rect)
             manager.process_events(event)
         manager.update(time_delta)
         manager.draw_ui(screen)
@@ -130,7 +179,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if pygame.mouse.get_focused():
+            pygame.mouse.set_visible(False)
+            all_sprites.update(pygame.mouse.get_pos())
+            all_sprites.draw(screen)
+            pygame.display.flip()
+        else:
+            pygame.display.flip()
+            screen.fill((235, 195, 155))
     screen.fill((235, 195, 155))
-    # pygame.draw.rect(screen, (0, 0, 0), (20, 20, 50, 50), 1)
-    pygame.display.flip()
 pygame.quit()
